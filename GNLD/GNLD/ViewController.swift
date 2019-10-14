cfdc261341c5ed972b85f4178c901424e59931d7
강수진 <rkdthd1234@naver.com>
2019-10-14 23:18:06 +0900

    inital project

---
+++ GNLD/GNLD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GNLD
+//
+//  Created by 강수진 on 2019/10/14.
+//  Copyright © 2019 강수진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
