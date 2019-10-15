185fb814fad7a79063bd3b9b0d518e057e126b5f
강수진 <rkdthd1234@naver.com>
2019-10-16 01:33:28 +0900

    모델 추가

---
+++ HM10 Serial/ArduSensor.swift
@@ -0,0 +1,24 @@
+//
+//  ArduSensor.swift
+//  Serial
+//
+//  Created by 강수진 on 2019/10/15.
+//  Copyright © 2019 Balancing Rock. All rights reserved.
+//
+
+import Foundation
+struct ArduSensor: Codable {
+    let temperature: Double
+    let humidityPercent: Double
+    let CO: Int
+    let pm10: Int
+    let pm2p5: Int
+    let soilPercent: Int
+
+    
+    enum CodingKeys: String, CodingKey {
+        case temperature, CO, pm10, pm2p5
+        case humidityPercent = "humidity_percent"
+        case soilPercent = "soil_percent"
+    }
+}
---
+++ HM10 Serial/UserData.swift
@@ -0,0 +1,42 @@
+//
+//  UserData.swift
+//  Serial
+//
+//  Created by 강수진 on 2019/10/16.
+//  Copyright © 2019 Balancing Rock. All rights reserved.
+//
+
+import Foundation
+
+enum UserDataKey: String {
+    case authorization = "authorization"
+    case lastSendDataTime = "lastSendDataTime"
+}
+
+struct UserData {
+    static func setUserDefault(value: Any, key: UserDataKey) {
+        UserDefaults.standard.set(value, forKey: key.rawValue)
+    }
+    static func getUserDefault<T>(key: UserDataKey, type: T.Type) -> T? {
+        return UserDefaults.standard.value(forKey: key.rawValue) as? T
+    }
+    static var isUserLogin: Bool {
+        if UserData.getUserDefault(key: .authorization, type: String.self) == nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    static var isOver30mSendData: Bool {
+        guard let lastSendDataTime = UserData.getUserDefault(key: .lastSendDataTime, type: Date.self) else {
+            return true
+        }
+        //현재가 마지막으로 보낸시간 보다 크면 다시 보내야함
+        //todo 5초 -> 60*30으로 바꿔야
+        if Date() > lastSendDataTime.addingTimeInterval(5) {
+           return true
+        } else {
+            return false
+        }
+    }
+}
